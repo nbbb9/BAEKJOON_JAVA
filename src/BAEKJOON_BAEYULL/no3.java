89165b5e4654c83e028fb7ea48302fc0cb2202a1
YoungWon <nbbb9@naver.com>
2024-02-01 21:10:49 +0900

    백준 문제풀이 _ 반목문 6,11,12(X) / 배열 3번 해결X 5까지 완료

--- src/BAEKJOON_BAEYULL/no3.java
+++ src/BAEKJOON_BAEYULL/no3.java
@@ -3,6 +3,8 @@ package BAEKJOON_BAEYULL;
 import java.util.Scanner;
 
 public class no3 {
+    //왜 막히는지 모르겠음.
+    //배열에 수를 입력할 때 입력한 수 보다(배열의 크기보다) 더 많은 수가 들어갈 수 있음.
     public static void main(String[] args) {
         //Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
@@ -15,39 +17,32 @@ public class no3 {
         //최솟값을 저장할 변수
         int min = 0;
 
-        if( N < 1 && N > 1000000){
+        if( N < 1 || N > 1000000){
             System.out.println("1부터 1000000까지만 입력하세요.");
-        }else{
-            for(int i = 0 ; i < N ; i++){
-                //입력한 N만큼 반복하면서, i가 0부터 N이 될 때까지 각 인덱스에 맞는값을 입력.
+        }else {
+            for (int i = 0; i < N; i++) {
+                //입력한 N만큼 반복하면서, i가 0부터 N이 될 때까지 순차적으로 값을 입력.
                 intarray[i] = sc.nextInt();
-                if( (intarray[i] < -1000000) && (intarray[i] > 1000000) ){
+                if (intarray[i] < -1000000 || intarray[i] > 1000000) {
                     System.out.println("-1000000부터 1000000까지만 입력하세요.");
-                    System.exit(0);
-                }else{
-                    continue;
                 }
             }
+        }
 
-            for(int i = 0 ; i < N ; i++){
-                //인덱스0부터 차례로 비교를 한다. 만약, max값이 해당 인덱스값보다 작다면
-                //해당 인덱스값을 max에 넣어준다.
-                if( max < intarray[i] ){
-                    max = intarray[i];
-                }else{
-                    continue;
-                }
+        for(int i = 0 ; i < N ; i++){
+            //인덱스0부터 차례로 비교를 한다. 만약, max값이 해당 인덱스값보다 작다면
+            //해당 인덱스값을 max에 넣어준다.
+            if( max < intarray[i] ){
+                max = intarray[i];
             }
+        }
 
-            for(int i = 0 ; i < N ; i++){
-                //인덱스 0부터 차례로 비교를 한다. 만약, min값이 해당 인덱스값보다 작다면
-                //해당 인덱스값을 min에 넣어준다.
+        for(int i = 0 ; i < N ; i++){
+            //인덱스 0부터 차례로 비교를 한다. 만약, min값이 해당 인덱스값보다 작다면
+            //해당 인덱스값을 min에 넣어준다.
+            min = intarray[i];
+            if( min > intarray[i] ){
                 min = intarray[i];
-                if( min < intarray[i] ){
-                    min = intarray[i];
-                }else{
-                    continue;
-                }
             }
         }
 

a1c3e9d5342b50f2de061b62c8196ce2cc7013f0
YoungWon <nbbb9@naver.com>
2024-02-08 17:46:34 +0900

    백준 문제풀이_6단계 3,5,7,8 X / 7단계 2번까지 완료

--- src/Step6/BAEKJOON_SHIMWHA1/no7.java
+++ src/Step6/BAEKJOON_SHIMWHA1/no7.java
@@ -1,29 +1,26 @@
 package Step6.BAEKJOON_SHIMWHA1;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class no7 {
     public static void main(String[] args) {
+        //Scanner 객체 생성
         Scanner scan = new Scanner(System.in);
-
+        //입력할 테스트 케이스 횟수를 저장하는 변수 N
         int N = scan.nextInt();
 
         int count = 0;
 
-        int prev = 0;
+        boolean bl = false;
 
         for(int i = 0 ; i < N ; i++){
-            String str = scan.nextLine();
-            int now = str.charAt(i);
+            String str = scan.next();
+            char [] chars = str.toCharArray();
 
-            if( prev == now ){
-                count++;
-                prev = now;
-            }//if end
+
+
         }//for end
 
-        System.out.println(count);
 
     }//main end
 }//class end

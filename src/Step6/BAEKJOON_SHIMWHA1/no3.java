b6819f2c48fd79c22c31362f945b25dcf5958566
YoungWon <nbbb9@naver.com>
2024-02-18 23:15:26 +0900

    백준 문제풀이 _ 6단계 3번 완료

--- src/Step6/BAEKJOON_SHIMWHA1/no3.java
+++ src/Step6/BAEKJOON_SHIMWHA1/no3.java
@@ -4,9 +4,24 @@ import java.util.Scanner;
 
 public class no3 {
     public static void main(String[] args) {
-        //Scanner 객체 생성
-        Scanner scan= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
 
+        for(int i = 1; i <= N ; i++) {
+            for(int j = 0; j < N-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for(int i = N-1; i >= 0 ; i--) {
+            for(int j = 0; j < N-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
 
     }//main end
 }//class end

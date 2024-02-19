e3d083064def76169e3ed67e0ba48a73c31d655e
YoungWon <nbbb9@naver.com>
2024-02-20 00:56:08 +0900

    백준 문제풀이_6단계 5,7,8 X / 8단계 2,5,6 X / 9단계 5,6 X / 10단계 3 X / 9단계까지 푼 문제들 주석 완료

--- src/Step9/BAEKJOON_DivisorMultiplePrimeNumbers/no2.java
+++ src/Step9/BAEKJOON_DivisorMultiplePrimeNumbers/no2.java
@@ -13,12 +13,16 @@ public class no2 {
         int N = Integer.parseInt(st.nextToken());
         //몇번째인지를 입력받는 변수 B
         int B = Integer.parseInt(st.nextToken());
+        //회차를 진행하면서 B와 값이 같은지 비교하기위한 변수 count
         int count = 0;
+        //결과값 출력을 위한 변수 result
         int result = 0;
-
+        //1부터 N까지 반복문 수행
         for(int i = 1 ; i <= N ; i++){
+            //만약 N나누기 i의 나머지가 0이라면 i는 약수이므로 count++ 수행
             if( N % i == 0 ){
                 count++;
+                //count가 B와 같다면 해당 약수(i)를 result에 저장하고 반복문 종료.
                 if( count == B ){
                     result = i;
                     break;
@@ -26,6 +30,7 @@ public class no2 {
             }//if end
         }//for end
 
+        //결과값 출력
         System.out.println(result);
 
     }//main end

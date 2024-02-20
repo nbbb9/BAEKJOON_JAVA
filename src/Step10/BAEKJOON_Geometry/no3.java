3d67a7095b810ce74560154f1f5bdbe8b076871a
YoungWon <nbbb9@naver.com>
2024-02-20 20:29:31 +0900

    백준 문제풀이_6단계 3,5,7,8X / 8단계 2,5,6X / 9단계 5,6X / 10단계 완료&주석필요

--- src/Step10/BAEKJOON_Geometry/no3.java
+++ src/Step10/BAEKJOON_Geometry/no3.java
@@ -8,26 +8,26 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class no3 {
-    //왜 틀리는지 모르겠음.
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+        //각 입력의 첫번째, 두번째값을 각각 저장할 리스트 X, Y
         List<Integer> X = new ArrayList<>();
         List<Integer> Y = new ArrayList<>();
-
-        for(int i = 0 ; i < 3 ; i++){
+        //세점의 좌표를 찍기위해 3번의 반복문 실행. 각각의 값을 리스트에 저장.
+        for(int i = 0  ; i < 3 ; i++){
             st = new StringTokenizer(reader.readLine(), " ");
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             X.add(x);
             Y.add(y);
-        }
+        }//for end
+        //입력 프로세스 종료
         reader.close();
-
-//        System.out.println(X);
-//        System.out.println(Y);
+        //각 리스트에 값이 올바르게 들어갔는지 테스트하기 위한 코드
+        //System.out.println(X);
+        //System.out.println(Y);
 
         int xtemp = Find(X);
         int ytemp = Find(Y);
@@ -36,14 +36,17 @@ public class no3 {
 
     }//main end
 
-    public static int Find(List<Integer> list){
+    public static int Find(List<Integer> list){//각 리스트에서 짝지어지지 않은 값을 찾기위한 메소드
+        //리스트의 0번 인덱스의 값을 temp에 넣는다.
         int temp = list.get(0);
-        if( list.get(0) == list.get(1) ){
+        //만약 0번 인덱스의 값이 1번인덱스의 값과 같다면,
+        //temp에 2번인덱스의 값을 넣는다.
+        if( list.get(0).equals(list.get(1))){
             temp = list.get(2);
-        }else{
-            if( list.get(0) == list.get(2) ){
+        }else{//만약 0번인덱스의 값이 1번인덱스의 값과 다르다면,
+            if(list.get(0).equals(list.get(2))){
                 temp = list.get(1);
-            }else{
+            }else {
                 temp = list.get(0);
             }
         }

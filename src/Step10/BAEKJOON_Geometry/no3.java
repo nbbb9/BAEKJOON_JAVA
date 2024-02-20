891e04746466efcf3f807d62e41ec2c3b70c1537
YoungWon <nbbb9@naver.com>
2024-02-21 00:51:09 +0900

    백준 문제풀이_10단계 3번 주석 완료. 틀렸던 이유 주석 완료.

--- src/Step10/BAEKJOON_Geometry/no3.java
+++ src/Step10/BAEKJOON_Geometry/no3.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 
 public class no3 {
     public static void main(String[] args) throws IOException {
-
+        //== 연산자와 equals메서드의 차이를 정확히 알자.
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         //각 입력의 첫번째, 두번째값을 각각 저장할 리스트 X, Y
@@ -29,9 +29,10 @@ public class no3 {
         //System.out.println(X);
         //System.out.println(Y);
 
+        //각각의 변수에 Find메서드를 통한 값 저장
         int xtemp = Find(X);
         int ytemp = Find(Y);
-
+        //결과값 출력
         System.out.println(xtemp + " " + ytemp);
 
     }//main end
@@ -43,13 +44,15 @@ public class no3 {
         //temp에 2번인덱스의 값을 넣는다.
         if( list.get(0).equals(list.get(1))){
             temp = list.get(2);
-        }else{//만약 0번인덱스의 값이 1번인덱스의 값과 다르다면,
+        }else{//만약 0번인덱스의 값이 1번인덱스의 값과 다르다면, 2가지의 경우의 수 발생
             if(list.get(0).equals(list.get(2))){
+                //만약, 0번인덱스의 값과 2번인덱스의 값이 같다면, 1번 인덱스의 값을 반환
                 temp = list.get(1);
             }else {
+                //0번인덱스와 2번인덱스가 다르다면, 0번 인덱스의 값 반환
                 temp = list.get(0);
-            }
-        }
+            }//if-else2 end
+        }//if-else end
         return temp;
     }//method Find end
 

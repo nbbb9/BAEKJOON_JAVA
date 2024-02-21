5d4d0d0ca7f63a2cddc2fdf84e2cbf0a58f35a60
YoungWon <nbbb9@naver.com>
2024-02-21 23:05:52 +0900

    백준 문제풀이_6단계 7번 도전중...

--- src/Step6/BAEKJOON_SHIMWHA1/no5.java
+++ src/Step6/BAEKJOON_SHIMWHA1/no5.java
@@ -44,27 +44,37 @@ public class no5 {
         //개수측정 배열의 값이 올바르게 들어갔는지 확인하기 위한 코드
 //        for(int i = 0 ; i < alphaAryInt.length ; i++){
 //            System.out.print(alphaAryInt[i] + " ");
-//        }
-
-        //가장 많은 알파벳의 개수를 저장하는 변수 temp
-        int temp = 0;
+//        }//for end
+        //가장 많은 알파벳의 개수를 저장하는 변수 maxAlpa
+        int maxAlpa = 0;
         //가장 많은 알파벳의 인덱스를 저장하는 변수 result
         int result = 0;
         //알파벳의 개수(26개)만큼 반복
         for(int i = 0 ; i < alphaAryInt.length ; i++){
             //만약, 각 알파벳의 개수가 0이 아니고 temp값이 그 개수보다 작다면,
             //temp에 해당개수를 넣고, result변수에 해당 인덱스를 저장한다.
-            if( alphaAryInt[i] != 0 && alphaAryInt[i] > temp){
-                //만약, 각 알파벳의 개수가 0이 아니고  이미 저장된 temp의 값과 같다면.
-                //물음표를 출력하고 시스템 종료.
-                if(alphaAryInt[i] == temp){
-                    System.out.println("?");
-                    System.exit(0);
-                }
-                temp = alphaAryInt[i];
+            if( alphaAryInt[i] != 0 && alphaAryInt[i] > maxAlpa) {
+                maxAlpa = alphaAryInt[i];
                 result = i;
-            }
+            }//if end
         }//for end
+
+        //최대값의 수가 동일한게 있는지 계산하기위한 변수 count.
+        //만약 반복문이 끝나고, count가 2이상이면 ?를 출력.
+        int count = 0;
+        //알파벳의 개수가 담긴 배열을 돌면서, 가장 많은 알파벳의 개수(temp)와 같다면 count++
+        for(int i = 0 ; i < alphaAryInt.length ; i++){
+            if( alphaAryInt[i] == maxAlpa ){
+                count++;
+            }//if end
+        }//for end
+        //count의 개수를 확인하기위한 코드
+        //System.out.println(count);
+        //만약, count가 2 이상이라면 2개이상의 알파벳이 같은 최대값을 가지므로 ?출력 후 시스템 종료
+        if( count >= 2 ){
+            System.out.println("?");
+            System.exit(0);
+        }//if end
         //가장 많은 알파벳의 종류가 하나일 경우, result에 저장된 인덱스값에 해당하는 알파벳 출력.
         System.out.println(alphaAry[result]);
 
--- src/Step6/BAEKJOON_SHIMWHA1/no7.java
+++ src/Step6/BAEKJOON_SHIMWHA1/no7.java
@@ -1,26 +1,50 @@
 package Step6.BAEKJOON_SHIMWHA1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class no7 {
-    public static void main(String[] args) {
-        //Scanner 객체 생성
-        Scanner scan = new Scanner(System.in);
-        //입력할 테스트 케이스 횟수를 저장하는 변수 N
-        int N = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
 
         int count = 0;
 
-        boolean bl = false;
-
         for(int i = 0 ; i < N ; i++){
-            String str = scan.next();
-            char [] chars = str.toCharArray();
-
-
-
+            String str = reader.readLine();
         }//for end
 
+        //BufferedReader 종료
+        reader.close();
 
     }//main end
+
+    public static boolean checkAlpa(String str){
+        boolean[] check = new boolean[26];
+        int prev = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            // i 번째 문자 저장 (현재 문자)
+            int now = str.charAt(i);
+            // 앞선 문자와 i 번째 문자가 같지 않다면? >> 해당부분 이해안됨.
+            if (prev != now) {
+                // 해당 문자가 처음 나오는 경우 (false 인 경우)
+                if ( check[now - 'a'] == false ) {
+                    // true 로 바꿔준다
+                    check[now - 'a'] = true;
+                    // 다음 턴을 위해 prev 도 바꿔준다
+                    prev = now;
+                }
+                // 해당 문자가 이미 나온 적이 있는 경우 (그룹단어가 아니게 됨)
+                else {
+                    //함수 종료
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
 }//class end

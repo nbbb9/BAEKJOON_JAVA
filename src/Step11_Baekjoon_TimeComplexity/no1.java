aabc0d95dbd96443696b3cabd01afe92c5d3e4c8
YoungWon <nbbb9@naver.com>
2024-02-23 00:11:52 +0900

    백준 문제풀이_8단계 2,5,6X / 9단계 6X / 11단계 도전중...

---
+++ src/Step11_Baekjoon_TimeComplexity/no1.java
@@ -0,0 +1,10 @@
+package Step11_Baekjoon_TimeComplexity;
+
+public class no1 {
+    public static void main(String[] args) {
+        //O(1)의 시간복잡도.
+        //어떤 수가 들어와도 값은 변하지 않음.
+        System.out.println(1);
+        System.out.println(0);
+    }//main end
+}//class end

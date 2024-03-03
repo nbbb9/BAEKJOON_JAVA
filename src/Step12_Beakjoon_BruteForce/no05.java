8a641814b610541bc1d5197e1e4c52c3ec462f70
YoungWon <nbbb9@naver.com>
2024-03-03 23:45:41 +0900

    백준 문제풀이_12단계 도전중...

---
+++ src/Step12_Beakjoon_BruteForce/no05.java
@@ -0,0 +1,15 @@
+package Step12_Beakjoon_BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        
+    }//main end
+}//class end

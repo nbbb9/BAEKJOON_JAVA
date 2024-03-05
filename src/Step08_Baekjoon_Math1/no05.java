d0a2d26a1eea6c98254d0e5881386bf0df348658
YoungWon <nbbb9@naver.com>
2024-03-06 01:54:28 +0900

    백준 문제풀이_8단계 5번 완료, 6X / 12단계 4,5X

--- src/Step08_Baekjoon_Math1/no05.java
+++ src/Step08_Baekjoon_Math1/no05.java
@@ -6,27 +6,31 @@ import java.io.InputStreamReader;
 
 public class no05 {
     //결국 하나의 외곽으로 나갈 때가 1회 지나는 것이다.
-    //처음 1번방부터 개수를 시작한다.
-    //2부터 6개의 숫자만큼 외곽의 크기가 증가한다.
+    //처음 1번방부터 화차를 시작한다.
+    //2부터 6개의 숫자만큼 외곽의 크기(개수)가 증가한다.
     //1 >> 6 >> 12 >> 18 >> 24 ......
     //각 외곽의 최대 숫자는 7, 19, 37, 61이다.
-    //이것의 규칙은, 최대숫자에 각 외각의 개수만큼 더한 값이다.
     public static void main(String[] args) throws IOException {
         //BuffredReader객체 생성
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        //
         int N = Integer.parseInt(reader.readLine());
-        //
-        int count = 0;
-
-        for(int i = 1 ; i <= N+6; i+=6){
-            if( N <= i ){
+        //몇번째 방인지 게산하기 위한 변수 count
+        int count = 1;
+        //각 외곽의 최소값을 저장하는 변수 edgeMin
+        int edgeMin = 2;
+        //만약 N이 1이라면 첫번째 방에서 멈추므로 1출력 후 종료.
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+            //N이 1보다 크다면 N이 각 외곽의 최소값(edgeMin)보다 작을 동안
+            //외곽의 최소값은 각 회차에 6을 곱한만큼 커지므로 edgeMin += (6 * count); 수행
+            //그리고 count++를 진행하여 몇번째 외곽인지 계산.
+            while (edgeMin <= N) {
+                edgeMin += (6 * count);
                 count++;
-                break;
-            }
-        }
-
-        System.out.println(count);
-
+            }//while end
+            //결과값 출력
+            System.out.println(count);
+        }//if-else end
     }//main end
 }//class end
--- src/Step08_Baekjoon_Math1/no06.java
+++ src/Step08_Baekjoon_Math1/no06.java
@@ -1,10 +1,14 @@
 package Step08_Baekjoon_Math1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class no06 {
-    public static void main(String[] args) {
-
-
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(reader.readLine());
+        reader.close();
 
     }//main end
 }//class end
